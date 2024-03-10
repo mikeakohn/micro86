4d2d888e6f316d0ec8081c147192c56c3d0c2ce4
Michael Kohn <mike@mikekohn.net>
2024-03-10 15:18:56 -0500

    Add tests for longer jumps.

--- test/jump.asm
+++ test/jump.asm
@@ -6,7 +6,7 @@ main:
   ;add eax, 1
   ;test eax, 2
 
-  add ebx, 1
+  ;add ebx, 1
   test ebx, 1
 
   jz skip_led
@@ -16,6 +16,25 @@ main:
   nop
   nop
 
+led_on_again:
+  mov ebx, 0x8008
+  mov eax, 1
+  mov [ebx], al
+  ret
+
+  hlt
+  hlt
+  hlt
+  hlt
+  hlt
+  hlt
+  hlt
+roar:
+  mov eax, 0xaa
+  hlt
+
+resb 128
+
 blah:
   mov eax, 10
   hlt
@@ -23,8 +42,18 @@ blah:
 
 skip_led:
   mov eax, 0xf1
+  call led_on_again
+  sub ebx, ebx
+  jz roar
   hlt
 
+  hlt
+  hlt
+  hlt
+  hlt
+  hlt
+  hlt
+  hlt
 led_on:
   ; 0x8008 is the LED.
   mov ebx, 0x8008
